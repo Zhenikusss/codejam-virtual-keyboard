1f09e9754bc572e8500552a38a14e388b88233c4
Yauheni Kozak <zhenikusss@gmail.com>
2020-04-06 20:44:14 +0300

    feat: add files project virtualKeyBoard

---
+++ script.js
@@ -0,0 +1,408 @@
+const keysEn = {
+  Backquote: '`',
+  Digit1: '1',
+  Digit2: '2',
+  Digit3: '3',
+  Digit4: '4',
+  Digit5: '5',
+  Digit6: '6',
+  Digit7: '7',
+  Digit8: '8',
+  Digit9: '9',
+  Digit0: '0',
+  Minus: '-',
+  Equal: '=',
+  Backspace: 'Backspace',
+  Tab: 'Tab',
+  KeyQ: 'q',
+  KeyW: 'w',
+  KeyE: 'e',
+  KeyR: 'r',
+  KeyT: 't',
+  KeyY: 'y',
+  KeyU: 'u',
+  KeyI: 'i',
+  KeyO: 'o',
+  KeyP: 'p',
+  BracketLeft: '[',
+  BracketRight: ']',
+  Backslash: '\\',
+  Delete: 'Del',
+  CapsLock: 'CapsLock',
+  KeyA: 'a',
+  KeyS: 's',
+  KeyD: 'd',
+  KeyF: 'f',
+  KeyG: 'g',
+  KeyH: 'h',
+  KeyJ: 'j',
+  KeyK: 'k',
+  KeyL: 'l',
+  Semicolon: ';',
+  Quote: '\'',
+  Enter: 'Enter',
+  ShiftLeft: 'Shift',
+  KeyZ: 'z',
+  KeyX: 'x',
+  KeyC: 'c',
+  KeyV: 'v',
+  KeyB: 'b',
+  KeyN: 'n',
+  KeyM: 'm',
+  Comma: ',',
+  Period: '.',
+  Slash: '/',
+  ArrowUp: '▲',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Ctrl',
+  MetaLeft: 'Win',
+  AltLeft: 'Alt',
+  Space: 'Space',
+  AltRight: 'Alt',
+  ArrowLeft: '◄',
+  ArrowDown: '▼',
+  ArrowRight: '►',
+  ControlRight: 'Ctrl',
+};
+const keysEnChar = {
+  Backquote: '~',
+  Digit1: '!',
+  Digit2: '@',
+  Digit3: '#',
+  Digit4: '$',
+  Digit5: '%',
+  Digit6: '^',
+  Digit7: '&',
+  Digit8: '*',
+  Digit9: '(',
+  Digit0: ')',
+  Minus: '_',
+  Equal: '+',
+  Backspace: 'Backspace',
+  Tab: 'Tab',
+  KeyQ: 'Q',
+  KeyW: 'W',
+  KeyE: 'E',
+  KeyR: 'R',
+  KeyT: 'T',
+  KeyY: 'Y',
+  KeyU: 'U',
+  KeyI: 'I',
+  KeyO: 'O',
+  KeyP: 'P',
+  BracketLeft: '{',
+  BracketRight: '}',
+  Backslash: '|',
+  Delete: 'Del',
+  CapsLock: 'CapsLock',
+  KeyA: 'A',
+  KeyS: 'S',
+  KeyD: 'D',
+  KeyF: 'F',
+  KeyG: 'G',
+  KeyH: 'H',
+  KeyJ: 'J',
+  KeyK: 'K',
+  KeyL: 'L',
+  Semicolon: ':',
+  Quote: '"',
+  Enter: 'Enter',
+  ShiftLeft: 'Shift',
+  KeyZ: 'Z',
+  KeyX: 'X',
+  KeyC: 'C',
+  KeyV: 'V',
+  KeyB: 'B',
+  KeyN: 'N',
+  KeyM: 'M',
+  Comma: '<',
+  Period: '>',
+  Slash: '?',
+  ArrowUp: '▲',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Ctrl',
+  MetaLeft: 'Win',
+  AltLeft: 'Alt',
+  Space: 'Space',
+  AltRight: 'Alt',
+  ArrowLeft: '◄',
+  ArrowDown: '▼',
+  ArrowRight: '►',
+  ControlRight: 'Ctrl',
+};
+const keysRu = {
+  Backquote: 'ё',
+  Digit1: '1',
+  Digit2: '2',
+  Digit3: '3',
+  Digit4: '4',
+  Digit5: '5',
+  Digit6: '6',
+  Digit7: '7',
+  Digit8: '8',
+  Digit9: '9',
+  Digit0: '0',
+  Minus: '-',
+  Equal: '=',
+  Backspace: 'Backspace',
+  Tab: 'Tab',
+  KeyQ: 'й',
+  KeyW: 'ц',
+  KeyE: 'у',
+  KeyR: 'к',
+  KeyT: 'е',
+  KeyY: 'н',
+  KeyU: 'г',
+  KeyI: 'ш',
+  KeyO: 'щ',
+  KeyP: 'з',
+  BracketLeft: 'х',
+  BracketRight: 'ъ',
+  Backslash: '\\',
+  Delete: 'Del',
+  CapsLock: 'CapsLock',
+  KeyA: 'ф',
+  KeyS: 'ы',
+  KeyD: 'в',
+  KeyF: 'а',
+  KeyG: 'п',
+  KeyH: 'р',
+  KeyJ: 'о',
+  KeyK: 'л',
+  KeyL: 'д',
+  Semicolon: 'ж',
+  Quote: 'э',
+  Enter: 'Enter',
+  ShiftLeft: 'Shift',
+  KeyZ: 'я',
+  KeyX: 'ч',
+  KeyC: 'с',
+  KeyV: 'м',
+  KeyB: 'и',
+  KeyN: 'т',
+  KeyM: 'ь',
+  Comma: 'б',
+  Period: 'ю',
+  Slash: '.',
+  ArrowUp: '▲',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Ctrl',
+  MetaLeft: 'Win',
+  AltLeft: 'Alt',
+  Space: 'Space',
+  AltRight: 'Alt',
+  ArrowLeft: '◄',
+  ArrowDown: '▼',
+  ArrowRight: '►',
+  ControlRight: 'Ctrl',
+};
+const keysRuChar = {
+  Backquote: 'Ё',
+  Digit1: '!',
+  Digit2: '"',
+  Digit3: '№',
+  Digit4: ';',
+  Digit5: '%',
+  Digit6: ':',
+  Digit7: '?',
+  Digit8: '*',
+  Digit9: '(',
+  Digit0: ')',
+  Minus: '_',
+  Equal: '+',
+  Backspace: 'Backspace',
+  Tab: 'Tab',
+  KeyQ: 'Й',
+  KeyW: 'Ц',
+  KeyE: 'У',
+  KeyR: 'К',
+  KeyT: 'Е',
+  KeyY: 'Н',
+  KeyU: 'Г',
+  KeyI: 'Ш',
+  KeyO: 'Щ',
+  KeyP: 'З',
+  BracketLeft: 'Х',
+  BracketRight: 'Ъ',
+  Backslash: '/',
+  Delete: 'Del',
+  CapsLock: 'CapsLock',
+  KeyA: 'Ф',
+  KeyS: 'Ы',
+  KeyD: 'В',
+  KeyF: 'А',
+  KeyG: 'П',
+  KeyH: 'Р',
+  KeyJ: 'О',
+  KeyK: 'Л',
+  KeyL: 'Д',
+  Semicolon: 'Ж',
+  Quote: 'Э',
+  Enter: 'Enter',
+  ShiftLeft: 'Shift',
+  KeyZ: 'Я',
+  KeyX: 'Ч',
+  KeyC: 'С',
+  KeyV: 'М',
+  KeyB: 'И',
+  KeyN: 'Т',
+  KeyM: 'Ь',
+  Comma: 'Б',
+  Period: 'Ю',
+  Slash: ',',
+  ArrowUp: '▲',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Ctrl',
+  MetaLeft: 'Win',
+  AltLeft: 'Alt',
+  Space: 'Space',
+  AltRight: 'Alt',
+  ArrowLeft: '◄',
+  ArrowDown: '▼',
+  ArrowRight: '►',
+  ControlRight: 'Ctrl',
+};
+
+function drawBody() {
+  const script = document.querySelector('script');
+  const wrapper = document.createElement('div');
+  wrapper.className = 'wrapper';
+  document.body.insertBefore(wrapper, script);
+}
+
+function drawInput() {
+  const input = document.createElement('div');
+  input.className = 'text-input';
+  input.innerHTML = '<textarea rows="15" cols="110"></textarea>';
+  document.querySelector('div.wrapper').append(input);
+}
+
+function drawKeyboardEn() {
+  const keyBoard = document.createElement('div');
+  keyBoard.className = 'keyboard';
+
+  for (key in keysEn) {
+    keyBoard.innerHTML += `<div class="key key-${key}">${keysEn[key]}</div>`;
+    document.querySelector('div.wrapper').append(keyBoard);
+  }
+}
+
+function textFocus() {
+  const textarea = document.querySelector('textarea');
+  textarea.focus();
+  textarea.addEventListener('blur', (e) => {
+    e.target.focus();
+  });
+}
+
+function createText() {
+  const text = document.createElement('div');
+  text.className = 'text';
+  text.innerHTML = 'Клавиатура создавалась в Windows <br> Комбинация клавиш для переключения языка: ';
+  document.querySelector('div.wrapper').append(text);
+}
+
+function drawChanges() {
+
+}
+
+function activeButton() {
+  const btn = document.querySelectorAll('div.key');
+  const textarea = document.querySelector('textarea');
+  const textareaSelectionStart = textarea.selectionStart;
+  const textareaSelectionEnd = textarea.selectionEnd;
+
+  btn.forEach((el) => {
+    el.addEventListener('mousedown', (e) => {
+      btn.forEach((el) => {
+        el.classList.remove('activeClick');
+      });
+      el.classList.add('activeClick');
+      if (el.innerHTML === 'Enter') {
+        textarea.value += '\n';
+      } else if (el.innerHTML === 'Tab') {
+        textarea.value += '\t';
+      } else if (el.innerHTML === 'Space') {
+        textarea.value += ' ';
+      } else if (el.innerHTML === 'Ctrl') {
+      } else if (el.innerHTML === 'CapsLock') {
+      } else if (el.innerHTML === 'Alt') {
+      } else if (el.innerHTML === 'Shift') {
+        console.log('shift');
+      } else if (el.innerHTML === 'Win') {
+      } else if (el.innerHTML === 'Backspace') {
+        textarea.value = textarea.value.slice(0, -1);
+      } else if (el.innerHTML === 'Del') {
+      } else {
+        textarea.value += el.innerHTML;
+      }
+    });
+  });
+  btn.forEach((el) => {
+    el.addEventListener('mouseup', () => {
+      btn.forEach((el) => {
+        el.classList.remove('activeClick');
+      });
+    });
+  });
+}
+
+function pressButton() {
+  const input = document.querySelector('div.text-input');
+  const textarea = document.querySelector('textarea');
+  input.addEventListener('keydown', (el) => {
+    if (el.shiftKey) {
+
+    }
+    el.preventDefault();
+    let count = 0;
+    for (key in keysEn) {
+      if (key === event.code) {
+        const btn = document.querySelectorAll('div.key');
+        btn[count].classList.add('active');
+        btn[count].classList.add('activeClick');
+
+        if (keysEn[key] === 'Enter') {
+          textarea.value += '\n';
+        } else if (keysEn[key] === 'Tab') {
+          textarea.value += '\t';
+        } else if (keysEn[key] === 'Space') {
+          textarea.value += ' ';
+        } else if (keysEn[key] === 'Ctrl') {
+        } else if (keysEn[key] === 'Alt') {
+        } else if (keysEn[key] === 'CapsLock') {
+        } else if (keysEn[key] === 'Shift') {
+          console.log('shift');
+          btn[count].classList.remove('active');
+        } else if (keysEn[key] === 'Backspace') {
+          textarea.value = textarea.value.slice(0, -1);
+        } else {
+          textarea.value += keysEn[key];
+        }
+      }
+      count++;
+    }
+  });
+  input.addEventListener('keyup', (el) => {
+    if (el.shiftKey === false) {
+
+    }
+    let count = 0;
+    for (key in keysEn) {
+      if (key === event.code) {
+        const btn = document.querySelectorAll('div.key');
+        btn[count].classList.remove('active');
+        btn[count].classList.remove('activeClick');
+      }
+      count++;
+    }
+  });
+}
+
+drawBody();
+drawInput();
+drawKeyboardEn();
+textFocus();
+activeButton();
+pressButton();
+createText();
